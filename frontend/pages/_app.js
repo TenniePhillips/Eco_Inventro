aa25291516c4ac8ec907a6d416ecf34c32fe0d34
Ahmed <donmarito5@gmail.com>
2024-05-08 09:20:38 +0100

    new

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,8 +2,7 @@ import "../styles/globals.css";
 import React, { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "antd/dist/reset.css";
-// import firebase from "../firebase";
-// import { message } from "antd";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   // const initMessage = () => {
@@ -23,6 +22,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ChakraProvider>
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   );
