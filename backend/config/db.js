0705891cb496ce6eb2fb4a51704cb9b685a33614
TenniePhillips <donmarito5@gmail.com>
2024-05-11 07:48:24 +0100

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,14 @@ const connectDb = async () => {
   try {
     mongoose.set("strictQuery", false);
 
-    const conns = await mongoose.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true,
-      autoIndex: true,
-      useNewUrlParser: true,
-    });
+    const conns = await mongoose.connect(
+      "mongodb+srv://devMosh:devMosh@cluster0.uu47p7v.mongodb.net/cluster0?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        autoIndex: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log(`Mongo DB connected: ${conns.connection.host}`);
     console.log(`Mongo DB name: ${conns.connection.name}`);
     // console.log(`Mongo DB host: ${conns.connection.host}`.purple);
