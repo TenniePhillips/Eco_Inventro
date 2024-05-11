27ba5a1893a69081ec7a1f854e4d5aa468472d77
TenniePhillips <donmarito5@gmail.com>
2024-05-11 06:28:50 +0100

    new

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import React, { useEffect } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useToast } from "@chakra-ui/react";
 import "antd/dist/reset.css";
 import { Analytics } from "@vercel/analytics/react";
 import useFcmToken from "../tools/useFcmToken";
@@ -9,25 +9,10 @@ import firebaseApp from "../firebase";
 // import { getTokens, onMessageListener } from "../firebase";
 
 function MyApp({ Component, pageProps }) {
-  // const [show, setShow] = useState(false);
-  // const [notification, setNotification] = useState({ title: "", body: "" });
-  // const [isTokenFound, setTokenFound] = useState(false);
-  // getTokens(setTokenFound);
-
-  // onMessageListener()
-  //   .then((payload) => {
-  //     setShow(true);
-  //     setNotification({
-  //       title: payload.notification.title,
-  //       body: payload.notification.body,
-  //     });
-  //     console.log(payload);
-  //   })
-  //   .catch((err) => console.log("failed: ", err));
-
   const { fcmToken, notificationPermissionStatus } = useFcmToken();
   // Use the token as needed
   fcmToken && console.log("FCM token:", fcmToken);
+  const toast = useToast();
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
@@ -35,8 +20,13 @@ function MyApp({ Component, pageProps }) {
       const messaging = getMessaging(firebaseApp);
       const unsubscribe = onMessage(messaging, (payload) => {
         console.log("Foreground push notification received:", payload);
-        // Handle the received push notification while the app is in the foreground
-        // You can display a notification or update the UI based on the payload
+        toast({
+          title: payload.notification.title,
+          description: payload.notification.body,
+          status: "info",
+          duration: 10000,
+          isClosable: true,
+        });
       });
       return () => {
         unsubscribe(); // Unsubscribe from the onMessage event
